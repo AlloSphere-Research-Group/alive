8e58ea2cd2a1ae3412075d4e0595fe7a3089bb7e
graham wakefield <grrrwaaa@gmail.com>
2012-09-25 18:04:11 -0700

    added submodules

--- vm/install_dependencies.sh
+++ vm/install_dependencies.sh
@@ -1,2 +1,19 @@
 #!/bin/bash  
 
+PRODUCT_NAME="alive"
+ROOT=`pwd`
+PLATFORM=`uname`
+ARCH=`uname -m`
+echo Installing dependencies for $PLATFORM $ARCH from $ROOT
+
+# grab a couple of external dependencies this way:
+cd ../
+git submodule init && git submodule update
+
+cd $ROOT/modules/luaclang
+./lake
+sudo ./lake install
+
+cd $ROOT/modules/luaosc
+./lake
+sudo ./lake install
