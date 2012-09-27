222ad8ee399179cccbf9174e61a28aa9e6d212b5
thecharlie <charlie@charlie-roberts.com>
2012-09-27 07:04:14 -0700

    added mdns to renderer and master

--- renderer/node_slave.js
+++ renderer/node_slave.js
@@ -3,19 +3,32 @@ var path 	= require('path');
 var io_in 	= require('socket.io').listen(8083);
 var exec 	= require('child_process').exec;
 var io	 	= require('socket.io-client');
+var mdns 	= require('mdns');
 
 var currentDir 	= __dirname;
+var MASTER_ADDRESS 	= null; //"127.0.0.1:8082";
+var master = null;
 
-var MASTER_ADDRESS 	= "127.0.0.1:8082";
+var browser = mdns.createBrowser(mdns.tcp('master'));
 
-var master = io.connect(MASTER_ADDRESS);
+browser.on('serviceUp', function(service) {
+  console.log("service up: ", service);
+  //console.log("ADDRESS ", service.addresses[0]);
+  // use host instead of ip address... addresses also returns MAC address of port
+  master = io.connect(service.host+":8082");
 
-master.on('connect', function(){ console.log("Connected to Master"); });
+  master.on('connect', function(){ console.log("Connected to Master"); });
 
-master.on('message', function(msg){ console.log("Recieved a message: " + msg); });
+  master.on('message', function(msg){ console.log("Recieved a message: " + msg); });
 
-master.on('disconnect', function(){ console.log("Disconnected from Master"); });
+  master.on('disconnect', function(){ console.log("Disconnected from Master"); });
 
-master.on('pull', function(obj) {
-	exec("git pull origin master", {cwd: currentDir}, function() { console.log("MADE A PULL!"); } );
+  master.on('pull', function(obj) {
+  	exec("git pull origin master", {cwd: currentDir}, function() { console.log("MADE A PULL!"); } );
+  });
 });
+
+browser.on('serviceDown', function(service) {
+  console.log("service down: ", service);
+});
+browser.start();
