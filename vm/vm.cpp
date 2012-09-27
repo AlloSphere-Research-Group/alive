54e8744da2d39e653d65fb39e118258bdb9e6053
graham wakefield <grrrwaaa@gmail.com>
2012-09-26 17:33:09 -0700

    updates

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -4,6 +4,7 @@
 #include "alloutil/al_Lua.hpp"
 #include "allocore/protocol/al_OSC.hpp"
 #include "allocore/system/al_Time.hpp"
+#include "allocore/io/al_Window.hpp"
 
 /* Apache Portable Runtime */
 #include "apr_general.h"
@@ -12,7 +13,6 @@
 #include "apr_network_io.h"
 #include "apr_time.h"
 
-#include "zmq.h"
 
 using namespace al;
 
@@ -21,95 +21,9 @@ al_sec OSC_TIMEOUT = 0.1;
 std::string serverIP = "127.0.01";
 Lua L;
 
-void * zcontext;
-
-//  Convert C string to 0MQ string and send to socket
-static int
-s_send (void *socket, const char *string) {
-    zmq_msg_t message;
-    zmq_msg_init_size (&message, strlen(string));
-    memcpy (zmq_msg_data (&message), string, strlen (string));
-    int size = zmq_send (socket, &message, 0);
-    zmq_msg_close (&message);
-    return (size);
-}
-
-class BackgroundThread : public ThreadFunction, public osc::PacketHandler {
-public:
-
-	BackgroundThread() 
-	:	receiver(8019),
-		sender(0),
-		active(true),
-		thread(*this) 
-	{
-		receiver.handler(*this);
-		
-		int rc;
-		
-		pub = zmq_socket(zcontext, ZMQ_PUB);
-		//zmq_setsockopt(sub, ZMQ_LINGER, 0, 0);
-		rc = zmq_connect (sub, "epgm://en0;239.255.1.1:5555");
-		
-//		rc = zmq_bind(pub, "tcp://*:5556");
-		if (rc) printf("error binding publisher: %s\n", zmq_strerror(rc));
-//		
-//		s_send(pub, "hello pubs");
-		
-		//sub = zmq_socket(zcontext, ZMQ_SUB);
-		//zmq_setsockopt(sub, ZMQ_LINGER, 0, 0);
-		//rc = zmq_connect (sub, "pgm://en0;239.255.1.1:5555");
-		//if (rc) printf("error connecting subscriber: %s\n", zmq_strerror(rc));
-		
-		//zmq_setsockopt(sub, ZMQ_SUBSCRIBE, "git");
-		
-	}
-	
-	virtual ~BackgroundThread() {
-		active = false;
-		thread.join();
-		if (sender) delete sender;
-		zmq_close(pub);
-	}
-	
-	virtual void onMessage(osc::Message& m) {
-		if (m.addressPattern() == "/git") {
-			std::string cmd;
-			m >> cmd;
-			printf("git %s\n", cmd.c_str());
-			// execute git pull
-		} else if (m.addressPattern() == "/handshake") {
-			m >> serverIP;
-			printf("Server IP is: %s\n", serverIP.c_str());
-			
-			sender = new osc::Send(8010, serverIP.c_str(), serverIP.size());
-		} else {
-			m.print();
-		}
-	}
-
-	virtual void operator()() {
-		while (active) {
-			// poll recv and handle commands
-			while(receiver.recv()){
-				al_sleep(OSC_TIMEOUT);
-			}
-			al_sleep(OSC_TIMEOUT);
-		}
-	}
-	
-	osc::Recv receiver;
-	osc::Send * sender;
-	bool active;
-	Thread thread;
-	
-	void * pub;
-	void * sub;
-};
+Window win;
 
 int main(int argc, char * argv[]) {
-
-	zcontext = zmq_init(1);
 
 	/*
 		This is a vm runtime launcher providing services
@@ -150,15 +64,14 @@ int main(int argc, char * argv[]) {
 	printf("starting\n");
 	
 	// implemented in C++?
-	BackgroundThread bt;
+	//BackgroundThread bt;
 	
-	while(1) {
-		al_sleep(1);
-	}
+	win.create();
+	win.startLoop();
 	
-	bt.active = 0;
+	//bt.active = 0;
 	
-	zmq_term(zcontext);
+	
 	
 	printf("bye\n");
 	return 0;
